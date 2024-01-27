b684d660c00351a09e441cd3fd06ddafcef56123
helitonns <helitonns@gmail.com>
2024-01-27 18:11:09 -0400

    Aula 0305

---
+++ src/03/05/App.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import useFetch from './useFetch';
+
+type Produto = {
+  id: string;
+  nome: string;
+  preco: number;
+  quantidade: number;
+  descricao: string;
+  internacional: boolean;
+}
+
+const App = () => {
+  const [id, setId] = React.useState("p001");
+  const produtos = useFetch<Produto[]>("https://data.origamid.dev/produtos");
+  const produto = useFetch<Produto>(`https://data.origamid.dev/produtos/${id}`);
+
+  return (
+  <section className="flex">
+    <div>
+      {produtos.data && produtos.data.map((produto)=> (
+        <button onClick={()=> setId(produto.id)} style={{fontSize: "1rem"}} key={produto.id}>{produto.id}</button>
+      ))}
+    </div>
+
+    <div>
+      {produto.data && (
+        <ul>
+          <li>ID: {produto.data.id}</li>
+          <li>Nome: {produto.data.nome}</li>
+          <li>Quantidade: {produto.data.quantidade}</li>
+          <li>Descricao: {produto.data.descricao}</li>
+          <li>Internacional: {produto.data.internacional ? "Sim" : "Nao"}</li>
+        </ul>
+      )}
+    </div>
+  </section>
+  );
+};
+
+export default App;
